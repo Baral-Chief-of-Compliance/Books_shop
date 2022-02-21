4b3a686781ce063bcf0ff2df427a3351bc932334
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-02-21 17:54:11 +0300

    21.02.2022 v1

--- cart/cart.py
+++ cart/cart.py
@@ -13,23 +13,53 @@ class Cart(object):
         self.cart = cart
 
 
-    # def __iter__(self):
-    #
-    #
-    #     book_id = self.cart.keys()
-    #     books = Book.objects.filter(id__in = book_id)
-    #
-    #     cart = self.cart.copy()
-    #     for book in books:
-    #         cart[str(book.id)]['book'] = book
-    #
-    #     for item in cart.values():
-    #         item['price'] = item['price']
-    #         item['total_price'] = item['price'] * item['']
+    def __iter__(self):
+
+        book_ids = self.cart.keys()
+        books = Book.objects.filter(id__in = book_ids)
+
+        cart = self.cart.copy()
+        for book in books:
+            cart[str(book.id)]['book'] = book
+
+        for item in cart.values():
+            item['price'] = item['price']
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
 
 
     def add(self, book, quantity = 1, update_quantity = False):
 
         book_id = str(book.id)
         if book_id not in self.cart:
-            self.cart[book_id] = {quantity}
+            self.cart[book_id] = {'quantity': 0, 'price': str(book.price)}
+
+        if update_quantity:
+            self.cart[book_id]['quantity'] = quantity
+        else:
+            self.cart[book_id]['quantity'] += quantity
+        self.save()
+
+
+    def save(self):
+        self.session.modified = True
+
+
+    def remove(self, book):
+
+        book_id = str(book.id)
+        if book_id in self.cart:
+            del self.cart[book_id]
+            self.save()
+
+    def get_total_price(self):
+        return sum(item['price']*item['quantity'] for item in self.cart.values())
+
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
