2db356024e07a7e48ce38b292a025359159c0343
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-02-22 15:18:22 +0300

    22.02.2022 v2

--- Books_Shop_Site/urls.py
+++ Books_Shop_Site/urls.py
@@ -3,4 +3,6 @@ from Books_Shop_Site import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('books-list/', views.BookClassView.as_view(), name = 'books-list'),
+    path('book-details/<int:pk>/', views.BookClassDetails.as_view(), name = 'book-detail'),
 ]
--- Books_Shop_Site/views.py
+++ Books_Shop_Site/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import View, ListView, DetailView
+from .models import Book
 
 
 def index(request):
     return render(request, 'Books_Shop_Site/index.html')
+
+
+class BookClassView(ListView):
+    models = Book
+    queryset = Book.objects.all()
+    template_name = "Books_Shop_Site/book_list.html"
+
+
+class BookClassDetails(DetailView):
+    model = Book
+    template_name = "Books_Shop_Site/book_detail.html"
