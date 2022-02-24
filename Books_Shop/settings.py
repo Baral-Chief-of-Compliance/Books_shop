d490b19fd6c78db044c0604e6731a39e90781f5d
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2022-02-25 00:46:54 +0300

    25.02.2022 v1

--- Books_Shop/settings.py
+++ Books_Shop/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Books_Shop_Site.apps.BooksShopSiteConfig',
+    'cart.apps.CartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Books_Shop_Site',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- Books_Shop/urls.py
+++ Books_Shop/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls')),
+    path('cart/', include('cart.urls',namespace='cart')),
     path('', include('Books_Shop_Site.urls')),
 ]
 
